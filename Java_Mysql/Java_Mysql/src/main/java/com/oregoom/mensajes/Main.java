19d194ba39eac4e53e3882389e6802a46fc620a9
Jose Vilchez <jhtgus971454@gmail.com>
2022-10-30 13:14:44 -0500

    1314JavaMsql

--- Java_Mysql/Java_Mysql/src/main/java/com/oregoom/mensajes/Main.java
+++ Java_Mysql/Java_Mysql/src/main/java/com/oregoom/mensajes/Main.java
@@ -5,7 +5,10 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-       Connection conectar = DriverManager.getConnection(
+        ListarRegistros();
+    }
+    static void ListarRegistros() throws SQLException{
+        Connection conectar = DriverManager.getConnection(
                "jdbc:mysql://localhost/mensajes_db?serverTimezone=UTC",
                "root",
                "53v1lch37"
@@ -14,8 +17,27 @@ public class Main {
        
         System.out.println("CONEXION EXITOSA");
         
+        String sql = "SELECT * FROM mensajes";
+        PreparedStatement ps = conectar.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
         
+        while (rs.next()){
+            int id = rs.getInt("id_mensaje");
+            String mensaje = rs.getString("mensaje");
+            String autor = rs.getString("autor");
+            String fecha = rs.getString("fecha");
+            
+            System.out.printf("%d %s %s %s\n", id, mensaje,autor,fecha);
+        }
+        rs.close();
+        ps.close();
+        conectar.close();
+
         }
     }
+    
+   
+    
+   
     
 
